14105b90d0018000b5cb4ae0bfde903fb4587a71
Khurrambek <xurrambeks@gmail.com>
2024-03-13 20:05:26 +0500

    fix: speed insights

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/app/components/Header";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -34,6 +35,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
               <Header />
               {children}
+              <SpeedInsights />
       </body>
     </html>
   );
